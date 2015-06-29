e216e8967f8d6188992ad2aa37cd02fa1760aa6e
jdyeakel <jdyeakel@gmail.com>
2015-06-29 17:10:03 -0600

    terminal function for SDP

--- Sevilleta_FM.R
+++ Sevilleta_FM.R
@@ -1,3 +1,24 @@
 library(Rcpp)
 
 #Sevilleta foraging model
+#Setup
+num_res <- 5
+
+#Probability of finding k resources of food type j
+m <- c(1,2,3,4,5)
+nu <- c(1,1,1,1,1)
+max_enc <- 50
+pk <- matrix(0,(max_enc+1),num_res)
+for (j in 1:num_res) {
+  for (k in 1:(max_enc+1)) {
+    pk[k,j] <-  dnbinom(k,mu = m[j], size = nu[j])
+  }
+  pk[,j] <- pk[,j] / sum(pk[,j])
+}
+
+sourceCpp("src/SDP_beq_func.cpp")
+
+#Run SDP
+Cout <- SDP_beq_func(
+  pk=pk,
+  Mc=5)
--- src/SDP_beq_func.cpp
+++ src/SDP_beq_func.cpp
@@ -8,6 +8,44 @@ using namespace Rcpp;
 // For more on using Rcpp click the Help button on the editor toolbar
 
 // [[Rcpp::export]]
-int timesTwo(int x) {
-   return x * 2;
+NumericMatrix SDP_beq_func(
+  NumericMatrix pk,
+  double Mc
+) {
+  
+  
+  //Define state variable max's and min's
+  //'State' refers to the value used in the calculation...
+  //Otherwise, the value is an integer and used in locating fitness values.
+  
+  //2750 kcal/kg
+  //~10 MJ in 2750 kcal.... so 10 MJ per kg.
+  double xmax_state = Mc*10; //Body size in kilograms to MegaJoules
+  int xmax = (int) floor(xmax_state);
+  double xc_state = 0.5*xmax_state;
+  int xc = (int) floor(xc_state);
+  
+  double thetamax_state = 500;
+  int thetamax = (int) thetamax_state;
+  
+  NumericMatrix Wterm(xmax,thetamax);
+  for (int i=xc;i<xmax;i++) {
+    for (int j=0;j<thetamax;j++) {
+      double x_state = (double) i+1;
+      double theta_state = (double) j+1;
+      Wterm(i,j) = 0.5*(2 - (xc_state/x_state) - (1/((0.01*theta_state)+1)));
+    }
+  }
+  double Wterm_max = Wterm(xmax-1,thetamax-1);
+  for (int i=xc;i<xmax;i++) {
+    for (int j=0;j<thetamax;j++) {
+      Wterm(i,j) = Wterm(i,j)/Wterm_max;
+    }
+  }
+  
+  
+  
+  
+  List Cout(5);
+  return Wterm;
 }
